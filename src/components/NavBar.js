31c46a1a7b6fefbccec561d352e902d352c690fc
Johnatan <jf.johnatan@gmail.com>
2021-03-19 12:09:01 +0700

    change NavBar design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,31 +3,36 @@ import styled from 'styled-components'
 
 const StyledNav = styled.nav`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
-  font-family: Concert One;
-  font-size: 1.5em;
+  font-family: "Open Sans";
+  font-size: 1.25em;
   background: #212121;
   width: 65%;
   min-width: 350px;
   max-width: 500px;
-  padding: 25px 0;
   margin: 0 auto;
   color: white;
+  border-radius: 0 0 5px 5px;
+  overflow: hidden;
 
   .active {
-    
     text-decoration: underline;
+    background-color: #333;
   }
 
   @media (max-width: 576px) {
     width: 100%; 
     max-width: none;
+    border-radius: 0;
   }
 `
 
-const StyledLinks = styled.span`
-  position: relative; 
+const StyledLink = styled.span`
+  position: relative;
+  display: inline-block;
+  height: 100%;
+  width: 50%;
+  padding: 12px 0;
+  text-align: center;
 
   &:hover {
     cursor: pointer;
@@ -37,19 +42,19 @@ const StyledLinks = styled.span`
 const NavBar = props => {
   return (
     <StyledNav>
-      <StyledLinks
+      <StyledLink
         className={ `${ props.mode === 'search' ? 'active' : '' }` }
         onClick={ () => props.setMode('search') }
       >
         Search
-      </StyledLinks>
+      </StyledLink>
 
-      <StyledLinks
+      <StyledLink
         className={ `${ props.mode === 'rankings' ? 'active' : '' }` }
         onClick={ () => props.setMode('rankings') }
       >
         Rankings
-      </StyledLinks>
+      </StyledLink>
     </StyledNav>
   )
 }
